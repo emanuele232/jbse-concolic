3797ce57bb89eafcdfb6ea4280c86339f9ea5d47
emanuele <emanuele.motto@gmail.com>
2018-05-10 14:18:15 +0200

    added HighPrioQueue

--- examples/common/Settings.java
+++ examples/common/Settings.java
@@ -4,13 +4,22 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public final class Settings {
-	//Pietro's settings
+	/*Pietro's settings
 	public static final Path JBSE_PATH      = Paths.get("/Users", "pietro", "git", "sushi", "jbse", "target", "classes");
 	public static final Path EVOSUITE_PATH  = Paths.get("/Users", "pietro", "git", "jbse-concolic", "lib", "evosuite-shaded-1.0.3.jar");
 	public static final Path EVOSUITE_MOSA_PATH = Paths.get("/Users", "pietro", "git", "jbse-concolic", "lib", "evosuite-shaded-1.0.6-SNAPSHOT.jar");
 	public static final Path SUSHI_LIB_PATH = Paths.get("/Users", "pietro", "git", "sushi", "runtime" , "target", "classes");
 	public static final Path Z3_PATH        = Paths.get("/opt", "local", "bin", "z3");
 	public static final Path WORKSPACE      = Paths.get("/Users/pietro/git/jbse-concolic/");
+	*/
+	
+	//Emanuele's settings
+	public static final Path JBSE_PATH      = Paths.get("/home", "emanuele", "Documenti","scuola", "Tesi", "sushi", "sushi", "jbse", "target", "classes");
+	public static final Path EVOSUITE_PATH  = Paths.get("/home", "emanuele", "git", "jbse-concolic", "lib", "evosuite-shaded-1.0.3.jar");
+	public static final Path EVOSUITE_MOSA_PATH = Paths.get("/home", "emanuele", "git", "jbse-concolic", "lib", "evosuite-shaded-1.0.6-SNAPSHOT.jar");
+	public static final Path SUSHI_LIB_PATH = Paths.get("/home", "emanuele", "Documenti","scuola", "Tesi", "sushi", "sushi", "runtime" , "target", "classes");
+	public static final Path Z3_PATH        = Paths.get("/home", "emanuele", "bin", "z3", "build", "z3");
+	public static final Path WORKSPACE      = Paths.get("/home/emanuele/git/jbse-concolic/");
 	
 	
 	//these are good for everyone
